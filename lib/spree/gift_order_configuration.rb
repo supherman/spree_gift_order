5e59b62c7b906c85aac4fee3341b3931bcfacfe2
Herman Moreno <herman.moreno@crowdint.com>
2014-03-25 14:32:36 -0600

    Add adjustmen advice

--- lib/spree/gift_order_configuration.rb
+++ lib/spree/gift_order_configuration.rb
@@ -3,5 +3,6 @@ module Spree
     preference :create_adjustment, :boolean, default: false
     preference :adjustment_amount, :decimal, default: 0
     preference :adjustment_label, :string, default: Spree.t(:gift_adjustment_label)
+    preference :adjustment_advice, :string, default: ''
   end
 end
